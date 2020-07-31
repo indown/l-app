fafa776f7291561f14b8fc8c40a2888c7f9aad4a
xiaoyang <ky6150450@gmail.com>
2020-07-31 16:30:16 +0800

    首页

---
+++ src/screens/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+interface HomeProps {}
+const Home: React.FunctionComponent<HomeProps> = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default Home;
